0f6a9a59bc1537902269e178e6a1abcdbe739339
Matthew Brien <matthewbrien@outlook.com>
2016-04-22 14:41:24 -0400

    readme.me and calendar grid

--- main.pl
+++ main.pl
@@ -11,8 +11,7 @@ use Tk::PNG; #display images in tk
 my $main_window;
 my $menu_bar;
 my $file_mb;
-my $help_mb;
-my $exit_mb;
+  my $calender_canvas;
 
 #draw the main window, menu bars, buttons etc.
 sub draw_window{
@@ -30,18 +29,31 @@ sub draw_window{
                                        -foreground=>'white'
                                       )->pack(-side=>'left');
 
-        $file_mb -> command(-label=>'help',
+        $file_mb -> command(-label=>'Help',
                           -background=>'black',
                           -activebackground=>'#202020',
                           -foreground=>'white',
                           -command=>sub{print("Helping...")}
                           );
-        $file_mb -> command(-label => 'import',
+        $file_mb -> command(-label => 'Import',
                             -background=>'black',
                             -activebackground=>'#202020',
                             -foreground=>'white',
                             -command=>sub{print("importing...")}
                             );
+
+       $file_mb -> command(-label => 'New Event',
+                           -background=>'black',
+                           -activebackground=>'#202020',
+                           -foreground=>'white',
+                           -command=>sub{print("importing...")}
+                           );
+        $file_mb -> command(-label => 'New Contact',
+                          -background=>'black',
+                          -activebackground=>'#202020',
+                          -foreground=>'white',
+                          -command=>sub{print("importing...")}
+                          );
         $file_mb -> command(-label=>'',
                             -background=>'black',
                             -activebackground=>'black');
@@ -51,10 +63,19 @@ sub draw_window{
                             -activebackground=>'#202020',
                             -foreground=>'white',
                             -command => sub{$main_window->destroy});
-        my $but = $main_window -> Button(-text=>"Push Me");
-      
+
+         $calender_canvas = $main_window->Canvas(-width=>550, -height=>250);
+        for(my $i =0; $i<7; $i++){
+          for(my $ii =0;$ii<7;$ii++){
+            my $button;
+            $button = $calender_canvas->Button(-text=>'button$i,$ii');
+            $button->grid(-column=>$i, -row=>$ii, -ipadx => 36, -ipady=>30,-sticky=>'nsew');
+          }
+        }
+        $calender_canvas->pack();
       }
 draw_window;
+
 MainLoop{
 
 }
